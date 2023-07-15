e2951828b0da1337de983202f04594c42b1f0193
noor <owies.h.hassan@gmail.com>
2023-07-15 17:54:57 +0300

    add pdf file

--- src/Components/SideBar/SideBar.js
+++ src/Components/SideBar/SideBar.js
@@ -38,7 +38,7 @@ const handleClick=()=>{
                    <motion.span variants={style.hoverIcons} whileHover='hover'onClick={handleClick}>   <NavLink  to='/' >  <HouseFill/><span className='text-icon'>Home</span></NavLink></motion.span>
                    <motion.span variants={style.hoverIcons} whileHover='hover' onClick={handleClick}> <NavLink to='/Project' > <PersonFill/><span className='text-icon'>Projects</span></NavLink></motion.span>
                    <motion.span  variants={style.hoverIcons} whileHover='hover'onClick={handleClick}> <NavLink to='/Profile'> <Wrench/><span className='text-icon'>Profile</span></NavLink></motion.span>
-                   <motion.span variants={style.hoverIcons} whileHover='hover' onClick={handleClick}>  <NavLink to="/Cv "   > <CardText/><span className='text-icon'>Cv</span></NavLink></motion.span>
+                   <motion.span variants={style.hoverIcons} whileHover='hover' onClick={handleClick}>  <NavLink to="/Cv "> <CardText/><span className='text-icon'>Cv</span></NavLink></motion.span>
                    <div>
                    </div>
                </div>
--- src/Pages/Cv/Cv.js
+++ src/Pages/Cv/Cv.js
@@ -1,34 +1,37 @@
 import {useEffect, useState} from "react";
-import { Document, Page,pdfjs } from 'react-pdf';
+
 import './Cv.css'
 import Loading from "../../Components/Loading/Loading";
-pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
-
 const Cv = () => {
     const [state, setState] = useState(true)
+    const [counter, setCounter] = useState(0)
 
+    useEffect(()=>{
+        const timer=setTimeout(()=>{
+            if (counter<10){
+                setCounter(prevState => prevState+1)
+            }else {
+                setState(false)
+            }
+        },500)
 
-
-  useEffect(()=>{
-      pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
-      setState(false)
-  },[])
-
+        return ()=>{
+            clearTimeout(timer)
+        }
+    },[counter])
 
 
     return (
-        <div className='CV.pdf'>
-            {state ? <Loading/> :
-             <div style={{display:'none'}}>
-                 <Document
-                     file="./Cv.pdf"
+        <div className='cv'>
+            {state && <Loading/>}
+            <iframe
 
-                 >
+                src="https://onedrive.live.com/embed?resid=3A4227ACE46EBE75%21170&amp;authkey=!AE1mpC9C9a1lV9U&amp;em=2"
+                style={{width: "100%", height: "100%", zIndex: 10}} frameBorder="0">This is an embedded <a
+                target="_blank" href="https://office.com">Microsoft
+                Office</a> document, powered by <a target="_blank" href="https://office.com/webapps">Office</a>.
+            </iframe>
 
-                 </Document>
-             </div>
-
-            }
 
 
         </div>
